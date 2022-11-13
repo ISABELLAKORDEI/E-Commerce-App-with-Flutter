e6cb22e19b5cdc2ed29333a1bc592ad0e02731c2
Hope Kagame <kagame458@protonmail.com>
2022-11-13 12:48:09 +0300

    Remove overflow error

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,28 +20,33 @@ class Homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(title: 'Zero to Unicorn'),
-      body: Column(children: [
-        CarouselSlider(
-          options: CarouselOptions(
-            aspectRatio: 1.5,
-            viewportFraction: 0.9,
-            enlargeCenterPage: true,
-            enlargeStrategy: CenterPageEnlargeStrategy.height,
-          ),
-          items: Category.categories
-              .map((category) => HeroCarouselCard(category: category))
-              .toList(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                aspectRatio: 1.5,
+                viewportFraction: 0.9,
+                enlargeCenterPage: true,
+                enlargeStrategy: CenterPageEnlargeStrategy.height,
+              ),
+              items: Category.categories
+                  .map((category) => HeroCarouselCard(category: category))
+                  .toList(),
+            ),
+            const SectionTitle(title: 'RECOMMENDED'),
+            ProductCarousel(
+                products: Product.products
+                    .where((product) => product.isRecommended)
+                    .toList()),
+            const SectionTitle(title: 'MOST POPULAR'),
+            ProductCarousel(
+                products: Product.products
+                    .where((product) => product.isPopular)
+                    .toList())
+          ],
         ),
-        const SectionTitle(title: 'RECOMMENDED'),
-        ProductCarousel(
-            products: Product.products
-                .where((product) => product.isRecommended)
-                .toList()),
-        const SectionTitle(title: 'MOST POPULAR'),
-        ProductCarousel(
-            products:
-                Product.products.where((product) => product.isPopular).toList())
-      ]),
+      ),
       bottomNavigationBar: const CustomNavBar(),
     );
   }
