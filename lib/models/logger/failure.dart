698f2b74c689b1ed8c0346989aa5b1f5f84debfc
Hope Kagame <kagame458@protonmail.com>
2023-01-06 03:44:46 +0300

    Add Failure models

---
+++ lib/models/logger/failure.dart
@@ -0,0 +1,48 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'failure.g.dart';
+part 'failure.freezed.dart';
+
+@freezed
+class ECommerceAppValidationError with _$ECommerceAppValidationError {
+  factory ECommerceAppValidationError(
+    int code,
+    List<ECommerceAppFieldValidationError> errors,
+  ) = _ECommerceAppValidationError;
+
+  factory ECommerceAppValidationError.fromJson(Map<String, dynamic> json) =>
+      _$ECommerceAppValidationErrorFromJson(json);
+}
+
+@freezed
+class ECommerceAppFieldValidationError with _$ECommerceAppFieldValidationError {
+  factory ECommerceAppFieldValidationError(
+    String key,
+    List<String> errors,
+  ) = _ECommerceAppFieldValidationError;
+
+  factory ECommerceAppFieldValidationError.fromJson(Map<String, dynamic> json) =>
+      _$ECommerceAppFieldValidationErrorFromJson(json);
+}
+
+class Failure implements Exception {
+  Failure({
+    required this.message,
+    this.errors = const [],
+  });
+
+  final String message;
+  List<ECommerceAppFieldValidationError> errors;
+
+  List<String> errorMessages() {
+    final _errors = <String>[];
+
+    for (final error in errors) {
+      _errors.add(
+        error.errors[0],
+      );
+    }
+
+    return List.castFrom<dynamic, String>(_errors);
+  }
+}
